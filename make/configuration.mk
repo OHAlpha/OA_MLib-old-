f3cf939e4cfebd5c86bb518417c0134a07b0babe
Lloyd Nugent Gibson <lnugentgibson@gmail.com>
2014-11-25 17:40:54 -0500

    Create configuration.mk
    sub-makefile containing directory definitions

---
+++ make/configuration.mk
@@ -0,0 +1,46 @@
+CC := gcc
+
+HeaderDir := include
+SourceDir := source
+TranslationDir := ../OA_MLib_out/processed
+AssemblyDir := assembly
+ObjectDir := objects
+ExecutableDir := bin
+ParameterDir := param
+ResultDir := results
+ArchiveDir := archives
+MakefileDir := make
+
+BuildDir := build
+TestDir := test
+
+BuildSourceDir := $(BuildDir)/$(SourceDir)
+BuildTranslationDir := $(TranslationDir)/$(BuildDir)
+BuildAssemblyDir := $(BuildDir)/$(AssemblyDir)
+BuildObjectDir := $(BuildDir)/$(ObjectDir)
+BuildExecutableDir := $(BuildDir)/$(ExecutableDir)
+BuildParameterDir := $(BuildDir)/$(ParameterDir)
+BuildResultDir := $(BuildDir)/$(ResultDir)
+
+TestHeaderDir := $(TestDir)/$(HeaderDir)
+TestSourceDir := $(TestDir)/$(SourceDir)
+TestTranslationDir := $(TranslationDir)/$(TestDir)
+TestAssemblyDir := $(TestDir)/$(AssemblyDir)
+TestObjectDir := $(TestDir)/$(ObjectDir)
+TestExecutableDir := $(TestDir)/$(ExecutableDir)
+TestResultDir := $(TestDir)/$(ResultDir)
+
+VPATH := $(HeaderDir):$(SourceDir):$(TranslationDir):$(AssemblyDir)
+VPATH := $(VPATH):$(ObjectDir):$(ArchiveDir):$(MakefileDir)
+
+VPATH := $(VPATH):$(BuildSourceDir):$(BuildTranslationDir):$(BuildAssemblyDir)
+VPATH := $(VPATH):$(BuildObjectDir):$(BuildExecutableDir):$(BuildParameterDir)
+VPATH := $(VPATH):$(BuildResultDir)
+
+VPATH := $(VPATH):$(TestHeaderDir):$(TestSourceDir):$(TestTranslationDir)
+VPATH := $(VPATH):$(TestAssemblyDir):$(TestObjectDir):$(TestExecutableDir)
+VPATH := $(VPATH):$(TestResultDir)
+
+#VPATH := $(VPATH)
+
+INCLUDE := -I$(HeaderDir) -I$(TestHeaderDir) -imacros $(HeaderDir)/macro.h
